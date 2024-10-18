d1121abaf85e3ad6268f12b77d7c3aa2c4661160
DaimarPP <Daimarpenso@gmail.com>
2024-10-18 14:04:08 -0400

    Creacion de estructura y variables

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+struct Persona {
+    char nombre;
+    char cedula;
+    int idRegistro;
+    int edad;
+    float altura;
+    char genero;
+};
